89c2418a5a2a8edc4b98ed1dd5cceca9133b9de6
Anthony <anthony.choi204@gmail.com>
2024-03-21 19:49:07 +1000

    Added my name to Code Network's turtle.

--- hello.py
+++ hello.py
@@ -1,8 +1,13 @@
 from turtle import *
+
 penup()
 home()
 screen = Screen()
 screen.setup(1000, 600)
 write("CODE NETWORK", align="center", font=("Arial", 24, "bold"))
+
+goto(-300,0)
+color("green")
+write("Anthony", align = "center", font = ("Times New Roman", 16))
 
 done()
