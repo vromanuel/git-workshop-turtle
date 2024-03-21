8f0ca907f1effbb59cf73e414cda5fe3e022811c
Sansika Nobody <fdool7@outlook.com>
2024-03-21 19:48:56 +1000

    added boop to turtle

--- hello.py
+++ hello.py
@@ -5,4 +5,9 @@ screen = Screen()
 screen.setup(1000, 600)
 write("CODE NETWORK", align="center", font=("Arial", 24, "bold"))
 
+goto(350, -200)
+color("medium purple")
+write("Boop", align="center", font=("Comic Sans", 9, "italic"))
+
+
 done()
