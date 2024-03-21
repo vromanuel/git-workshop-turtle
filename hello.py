e1cb3258ee53987ce55d852ea02ceac5fc00ce78
GodfreyLee <godfreylee2000@gmail.com>
2024-03-21 19:48:22 +1000

    Update

--- hello.py
+++ hello.py
@@ -5,4 +5,13 @@ screen = Screen()
 screen.setup(1000, 600)
 write("CODE NETWORK", align="center", font=("Arial", 24, "bold"))
 
+
+goto (88,88)
+color("green")
+write("Ggoofy", align="center", font=("Times New Roman", 24, "bold"))
+for num in (range(100)):
+    forward(10)
+    left(10)
+
+
 done()
