fc28db101b5d9eae96f13491e06f6f5c43b26590
Lexiss <62432426+lexiss42@users.noreply.github.com>
2024-03-21 09:45:08 +0000

    add my name

--- hello.py
+++ hello.py
@@ -5,4 +5,10 @@ screen = Screen()
 screen.setup(1000, 600)
 write("CODE NETWORK", align="center", font=("Arial", 24, "bold"))
 
+up()
+goto(300,-200)
+color("pink")
+down()
+write("Alex O'Donnell", align="center", font=("Comic Sans MS", 16))
+
 done()
