de4aa29015ee4d52825ade6696ed22c24e1cde4e
kc981211 <yikyu98@gmail.com>
2024-03-21 19:49:22 +1000

    turtle

--- hello.py
+++ hello.py
@@ -5,4 +5,12 @@ screen = Screen()
 screen.setup(1000, 600)
 write("CODE NETWORK", align="center", font=("Arial", 24, "bold"))
 
+goto(400, 298)
+shape("turtle")
+color("yellow")
+write("Kim", align='center', font=("Arial", 12, "normal"))
+for i in range(4):
+    forward(100)
+    left(90)
+
 done()
