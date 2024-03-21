b8fc15d4b32c7a1f863b13dd563735ee9e06784a
Cool-Bu1lder <omnical@outlook.com>
2024-03-21 19:47:58 +1000

    add Daniel to the turtle

--- hello.py
+++ hello.py
@@ -5,4 +5,8 @@ screen = Screen()
 screen.setup(1000, 600)
 write("CODE NETWORK", align="center", font=("Arial", 24, "bold"))
 
+goto(250, 150)
+color("black")
+write("Daniel Willoughby", align='centre', font=("Verdana", 18))
+
 done()
